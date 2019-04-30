768627307dc223678b0d79eb9dab92774a254f59
Brendon Roy <brendonroy@hotmail.ca>
2019-04-30 09:16:06 -0400

    button for high screen

--- BrickBreaker/Screens/HighScreen.Designer.cs
+++ BrickBreaker/Screens/HighScreen.Designer.cs
@@ -28,22 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(291, 292);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Main Menu";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-
-			this.label1 = new System.Windows.Forms.Label();
 			this.highScores = new System.Windows.Forms.Label();
+			this.backButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// highScores
@@ -58,24 +44,42 @@
 			this.highScores.TabIndex = 5;
 			this.highScores.Text = "1.\r\n\r\n2.\r\n\r\n3.\r\n\r\n4.\r\n\r\n5.\r\n\r\n6.\r\n\r\n7.\r\n\r\n8.\r\n\r\n9.\r\n\r\n10.";
 			// 
+			// backButton
+			// 
+			this.backButton.BackgroundImage = global::BrickBreaker.Properties.Resources.button;
+			this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.backButton.FlatAppearance.BorderSize = 0;
+			this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.backButton.Font = new System.Drawing.Font("OCR A Std", 14F);
+			this.backButton.ForeColor = System.Drawing.Color.Cyan;
+			this.backButton.Location = new System.Drawing.Point(24, 458);
+			this.backButton.Name = "backButton";
+			this.backButton.Size = new System.Drawing.Size(119, 58);
+			this.backButton.TabIndex = 6;
+			this.backButton.Text = "BACK";
+			this.backButton.UseVisualStyleBackColor = true;
+			this.backButton.Click += new System.EventHandler(this.backButton_Click);
+			// 
 			// HighScreen
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Transparent;
 			this.BackgroundImage = global::BrickBreaker.Properties.Resources.Stars;
+			this.Controls.Add(this.backButton);
 			this.Controls.Add(this.highScores);
-			this.Controls.Add(this.label1);
 			this.Name = "HighScreen";
 			this.Size = new System.Drawing.Size(800, 550);
 			this.Load += new System.EventHandler(this.HighScreen_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
+
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label highScores;
+		private System.Windows.Forms.Button backButton;
 	}
 }
